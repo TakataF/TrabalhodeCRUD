c000e4827cb7d9573e88ec1ee242813263607d07
Felipe Takata de Alvarenga <felipetakatadealvarenga@gmail.com>
2022-09-18 16:38:23 -0300

    Feat alterar e listar produto,fornecedor

---
+++ app/src/main/java/com/takata/trabalhodecrud/AlterarFornecedor.java
@@ -0,0 +1,14 @@
+package com.takata.trabalhodecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlterarFornecedor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_fornecedor);
+    }
+}
---
+++ app/src/main/java/com/takata/trabalhodecrud/AlterarProduto.java
@@ -0,0 +1,14 @@
+package com.takata.trabalhodecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlterarProduto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_produto);
+    }
+}
