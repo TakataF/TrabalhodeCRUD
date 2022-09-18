479d6e9c2fd87406018a2676be181f8faec32757
Felipe Takata de Alvarenga <felipetakatadealvarenga@gmail.com>
2022-09-18 16:19:59 -0300

    Feat cadastro fornecedor, produto

--- app/src/main/java/com/takata/trabalhodecrud/TelaCadastroFornecedor.java
+++ app/src/main/java/com/takata/trabalhodecrud/TelaCadastroFornecedor.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TelaCadastro extends AppCompatActivity {
+public class TelaCadastroFornecedor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_cadastro);
+        setContentView(R.layout.activity_tela_cadastro_fornecedor);
     }
 }
